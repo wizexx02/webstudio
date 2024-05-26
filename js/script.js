6b3d8ea60fdd8e083c98ed94f03c6c8a2c3fbf35
wizexx02 <dvladvlad11@gmail.com>
2024-05-26 09:46:07 +0300

    add cde

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+(() => {
+  const refs = {
+  openMenuBtn: document.querySelector('.header__button'),
+  closeMenuBtn: document.querySelector('.burg__menu-button'),
+  menu: document.querySelector('.burg__menu'),
+  };
+  
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  function toggleMenu() {
+  refs.menu.classList.toggle('is-hidden');
+  }
+  })();
